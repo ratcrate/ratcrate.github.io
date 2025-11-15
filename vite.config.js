51ba7d77dc6e4c052422eda6c8e5f9d2aa5466c4
rvbug <rakesh.venkat@gmail.com>
2025-11-15 18:09:32 +0530

    fix base

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+//
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/'
+// })
+
+// vite.config.js
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// CRITICAL: The base URL for a User/Organization site must be '/'.
 export default defineConfig({
   plugins: [react()],
-  base: '/'
+  base: '/', 
 })
